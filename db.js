d0ed530a3bd32661b9b4969d4de91bd6f3726f48
putra443 <putra443@gmail.com>
2024-04-08 18:39:35 +0700

    update db & env

--- db.js
+++ db.js
@@ -1,12 +1,19 @@
-import { Pool } from 'pg';
+// import { Pool } from 'pg';
+
+// const pool = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: `perangkat lunak simulasi frs`,
+//   password: 'fransiskus31',
+//   port: 5432,
+// });
+import pg from 'pg';
+
+const { Pool } = pg;
 
 const pool = new Pool({
-  user: 'postgres',
-  host: 'localhost',
-  database: `perangkat lunak simulasi frs`,
-  password: 'fransiskus31',
-  port: 5432,
-});
+  connectionString: process.env.POSTGRES_URL ,
+})
 
 
 export async function query(text,params){
